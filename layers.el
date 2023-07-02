f01785827bb7ca983e3a80216101ed825deaae73
Lycea <20510874+Lycea@users.noreply.github.com>
2023-07-02 02:57:40 +0200

    Adjusted sample layer
    - renamed init to layers, for best practive
    - removed function inclusion from layers, since unneeded

--- layers.el
+++ layers.el
@@ -1,5 +1,5 @@
 ;; Load the functions from funcs.el
-(load-file (concat (file-name-directory load-file-name) "funcs.el"))
+;;(load-file (concat (file-name-directory load-file-name) "funcs.el"))
 
 ;; Provide the layer
 (configuration-layer/declare-layer 'template-layer)
